6d36da80bf07200d72244c80a649268221f59f98
jasmiinv <jasmiinv@metropolia.fi>
2024-03-05 13:21:16 +0200

    login files

--- App.tsx
+++ App.tsx
@@ -1,23 +1,14 @@
 import {StatusBar} from 'expo-status-bar';
-import {Platform, SafeAreaView, StyleSheet, Text} from 'react-native';
+import Navigator from './src/navigators/Navigator';
+import {UserProvider} from './src/contexts/UserContext';
 
 const App = () => {
-  console.log('moro');
   return (
-    <SafeAreaView style={styles.container}>
-      <Text>HeLLO!</Text>
+    <UserProvider>
+      <Navigator />
       <StatusBar style="auto" />
-    </SafeAreaView>
+    </UserProvider>
   );
 };
 
 export default App;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    paddingTop: Platform.OS === 'android' ? 30 : 0,
-  },
-});
